65737f7d11310da476bae46bbc1a9c9d1b8bd898
xxal <leckalmeida@gmail.com>
2026-01-09 08:21:16 -0300

    feat: add initial database migration

---
+++ FinancialChat.Infrastructure/Persistence/ChatDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FinancialChat.Infrastructure.Persistence;
+
+public class ChatDbContextFactory
+    : IDesignTimeDbContextFactory<ChatDbContext> {
+    public ChatDbContext CreateDbContext(string[] args) {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ChatDbContext>();
+
+        var connectionString =
+            configuration.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ChatDbContext(optionsBuilder.Options);
+    }
+}
