96cc5a33ca46f6826a233e568a30c62eded8f3d8
xxal <leckalmeida@gmail.com>
2026-01-09 01:34:36 -0300

    feat: add core chat domain entities

---
+++ FinancialChat.Domain/Entities/ChatMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialChat.Domain.Entities;
+
+public class ChatMessage {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public Guid ChatRoomId { get; private set; }
+    public string UserName { get; private set; } = null!;
+    public string Content { get; private set; } = null!;
+    public DateTime CreatedAt { get; private set; }
+    public bool IsFromBot { get; private set; }
+
+    protected ChatMessage() { }
+
+    public ChatMessage(
+        Guid chatRoomId,
+        string userName,
+        string content,
+        bool isFromBot = false) {
+        ChatRoomId = chatRoomId;
+        UserName = userName;
+        Content = content;
+        IsFromBot = isFromBot;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
+
+
---
+++ FinancialChat.Domain/Entities/ChatRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialChat.Domain.Entities;
+
+public class ChatRoom {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; private set; } = null!;
+
+    protected ChatRoom() { }
+
+    public ChatRoom(string name) {
+        Name = name;
+    }
+}
