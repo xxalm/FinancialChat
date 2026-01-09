f397ddd6921e32e93fed5adf63edd96be09e4e91
xxal <leckalmeida@gmail.com>
2026-01-09 01:46:33 -0300

    feat: add application contracts and send chat message

---
+++ FinancialChat.Application/Interfaces/IChatNotifier.cs
@@ -0,0 +1,9 @@
+﻿using FinancialChat.Domain.Entities;
+
+namespace FinancialChat.Application.Interfaces;
+
+public interface IChatNotifier {
+    Task NotifyMessageAsync(
+        ChatMessage message,
+        CancellationToken cancellationToken = default);
+}
